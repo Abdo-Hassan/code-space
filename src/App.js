f5dad55c426f22b7b98163299bd94ab0f351fbef
abdo hassan <abdocoder0@gmail.com>
2020-09-23 01:07:50 +0200

    added layout & some styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import React from 'react';
+import React, { useState, Fragment } from 'react';
 import './App.css';
+import Editor from './components/Editor';
 
 function App() {
+  const [html, setHtml] = useState('');
+  const [css, setCss] = useState('');
+  const [javascript, setJavascript] = useState('');
   return (
-    <div className='App'>
-      <h1>Code Space</h1>
-    </div>
+    <Fragment>
+      <div className='pane top-pane'>
+        <Editor
+          language='xml'
+          displayName='HTML'
+          value={html}
+          onChange={setHtml}
+        />
+        <Editor
+          language='xml'
+          displayName='CSS'
+          value={css}
+          onChange={setCss}
+        />
+        <Editor
+          language='xml'
+          displayName='Javascript'
+          value={javascript}
+          onChange={setJavascript}
+        />
+      </div>
+      <div className='pane'>
+        <iframe
+          title='output'
+          sandbox='allow-scripts'
+          frameBorder='0'
+          width='100%'
+          height='100%'
+        ></iframe>
+      </div>
+    </Fragment>
   );
 }
 
