72a887a5a857428b527829d7e862baf30722396e
abdo hassan <abdocoder0@gmail.com>
2020-09-23 19:38:46 +0200

    added more options

--- src/components/Editor.js
+++ src/components/Editor.js
@@ -4,6 +4,18 @@ import 'codemirror/theme/material.css';
 import 'codemirror/mode/xml/xml';
 import 'codemirror/mode/css/css';
 import 'codemirror/mode/javascript/javascript';
+import 'codemirror/addon/hint/show-hint';
+import 'codemirror/addon/hint/javascript-hint';
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror/addon/hint/anyword-hint';
+import 'codemirror/keymap/sublime';
+import 'codemirror/addon/edit/closebrackets';
+import 'codemirror/addon/edit/closetag';
+import 'codemirror/addon/fold/foldcode';
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+import 'codemirror/addon/fold/comment-fold';
+import 'codemirror/addon/fold/foldgutter.css';
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCompressAlt, faExpandAlt } from '@fortawesome/free-solid-svg-icons';
@@ -37,6 +49,17 @@ const Editor = ({ displayName, language, value, onChange }) => {
           mode: language,
           lineNumbers: true,
           theme: 'material',
+          autofocus: true,
+          smartIndent: true,
+          foldGutter: true,
+          gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+          autoCloseTags: true,
+          keyMap: 'sublime',
+          matchBrackets: true,
+          autoCloseBrackets: true,
+          extraKeys: {
+            'Ctrl-Space': 'autocomplete',
+          },
         }}
       />
     </div>
